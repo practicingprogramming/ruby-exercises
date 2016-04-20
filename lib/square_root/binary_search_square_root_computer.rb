843eaa8db6c99a0197d7b0e46bdb112421860c67
practicingprogramming <practicingprogramming@gmail.com>
2016-04-19 21:42:32 -0700

    Cleanup binary search

--- lib/square_root/binary_search_square_root_computer.rb
+++ lib/square_root/binary_search_square_root_computer.rb
@@ -1,7 +1,6 @@
 module SquareRoot
   # Calculate square root by using binary search.
   class BinarySearchSquareRootComputer
-    # TODO: handle negatives
     def get_root(x, precision)
       validate_input(x, precision)
       if x >= 1
@@ -9,10 +8,10 @@ module SquareRoot
       else
         left, right = x.to_f, 1
       end
-      prev = right + 1000
+      prev = nil
       loop do
         middle = (left + right) / 2
-        return middle if (prev - middle).abs < precision / 2
+        return middle if finished?(middle, prev, precision)
         prev = middle
         if middle**2 > x
           right = middle
@@ -27,6 +26,10 @@ module SquareRoot
     def validate_input(x, precision)
       fail ArgumentError.new("Invalid precision: #{precision}, must be positive") unless precision > 0
       fail ArgumentError.new("Invalid input: #{x}, must be non-negative") unless x >= 0
+    end
+
+    def finished?(current, previous, precision)
+      !previous.nil? && (current - previous).abs < precision.to_f / 2
     end
   end
 end
